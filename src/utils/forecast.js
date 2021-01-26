1083adfa7e36586bc164bde7f99af649b9f194fc
Prapti Maheshwari <praptimaheshwari8@gmail.com>
2021-01-26 20:45:24 +0530

    changes done

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = ( latitute,longitute, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
-            callback(undefined, 'It is currently  ' + body.current.temperature + '  degrees Fahrenheit out ,   '+ body.current.weather_descriptions[0] + '.   There is   ' + body.current.precip + ' % chance of rain and '+body.current.humidity + ' %humidity.' )
+            callback(undefined, body.current.weather_descriptions[0] + '  It is currently  ' + body.current.temperature + '  degrees  out ,It feels like   '+ body.current.feelslike + '   degree out . There is   ' + body.current.precip + '% chance of rain and humidity is '+body.current.humidity + ' %.' )
            
         }
     })
